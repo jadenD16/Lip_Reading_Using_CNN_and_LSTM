95022ab5a667b0af7460e28176ef0f7d13153d78
jadenD <delmojason@gmail.com>
2018-12-07 04:15:08 -0800

    Create cnn_model.py

---
+++ neural_network/cnn_model.py
@@ -0,0 +1,55 @@
+import os
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.activations import
+from keras import optimizers
+
+# dimensions of our images.
+img_width, img_height = 180, 144
+
+train_data_dir = 'data/train'
+validation_data_dir = 'data/validation'
+
+# used to rescale the pixel values from [0, 255] to [0, 1] interval
+datagen = ImageDataGenerator(rescale=1./255)
+
+# automagically retrieve images and their classes for train and validation sets
+train_generator = datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=16,
+        class_mode='binary')
+
+validation_generator = datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=32,
+        class_mode='binary')
+
+model = Sequential()
+model.add(Convolution2D(32, 3, 3, input_shape=(img_width, img_height,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Convolution2D(32, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Convolution2D(64, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
