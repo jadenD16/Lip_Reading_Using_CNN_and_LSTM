78edd404b2af65fa9156cbb425ecccb003d3fefc
jadenD <delmojason@gmail.com>
2019-03-01 22:08:00 +0800

    try the sample run

--- sampleRunning.py
+++ sampleRunning.py
@@ -14,30 +14,58 @@ from keras.callbacks import EarlyStopping, TensorBoard
 from keras.models import load_model
 from keras.models import model_from_json
 from sklearn.utils import shuffle
+import tensorflow as tf
+from keras.models import Sequential
+from keras.models import load_model
+from keras.layers.core import Dense, Dropout, Activation, Masking
+from keras.layers import TimeDistributed
+import gc
+from keras.optimizers import rmsprop
+
+
 
 # p = our pre-treined model directory, on my case, it's on the same script's diretory.
 p = "C:\\Users\\Jaden\\Downloads\\shape_predictor_68_face_landmarks.dat"
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(p)
 
-cap = cv2.VideoCapture('D:\\Datasets\\s1\\bbaf2n.mpg')
-
 counter = 0
 
-weight,height = (40,40)
+path = 'C:/Users/Jaden/PycharmProjects/Thesis/Lip_Reading_Using_CNN_and_LSTM/training/model'
 
-path = 'C:/Users/Jaden/PycharmProjects/Thesis/Lip_Reading_Using_CNN_and_LSTM/training/2nd Model checkpoint(ac- 78 and valacc - 63/model.h5'
+def normalizedMouth(mouth):
 
-def read_model(filepath,weights_filename='untrained_weight.h5',
-               topo_filename='untrained_topo.json'):
-    print("Reading Model from " + weights_filename + " and " + topo_filename)
+    image_min = mouth[mouth > 0].min()
+    image_max = mouth[mouth > 0].max()
+
+    mouth = (mouth - image_min) / (float(image_max - image_min))
+
+    return mouth
+
+def read_model(weights_filename='untrained_weight.h5',
+               mod='model-2.h5'):
+
+    global path
+
+    print("Reading Model from " + weights_filename + " and " + mod)
     print("Please wait, it takes time.")
-    model = load_model(path)
+    model = load_model(path+'/'+mod)
     #model.load_weights(weights_filename)
     print("Finish Reading!")
-    rmsprop(lr=0.0001, rho=0.9, epsilon=1e-6)
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
+
+cap = cv2.VideoCapture(path)
+x2 = 0
+y2 = 0
+w2 = 0
+h2 = 0
+counter = 0
+
+wordSequence = np.zeros((40, 1600))
+wCounter = 0
+
+model = read_model()
 
 while True:
     # Getting out image by webcam
@@ -53,37 +81,53 @@ while True:
     mask = np.zeros(image.shape, dtype="uint8")
 
     counter+= 1
-    time = cap.get(cv2.CAP_PROP_POS_MSEC)
+    time = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+
+    if counter == time:
+            break
 
     # For each detected face, find the landmark.
     for (i, rect) in enumerate(rects):
-        # Make the prediction and transfom it to numpy array
+    # Make the prediction and transfom it to numpy array
         shape = predictor(gray, rect)
         shape = face_utils.shape_to_np(shape)
 
-        minn = np.asarray(shape[48:68])
 
-        min2 = minn.min(0)
-        max2 = minn.max(0)
+        if counter == 1:
+            minn = np.asarray(shape[48:68])
 
-        x2,y2=min2
-        w2,h2 = max2
+            min2 = minn.min(0)
+            max2 = minn.max(0)
 
-        cropped = image[y2:h2,x2:w2]
+            x2, y2 = min2
+            w2, h2 = max2
 
+            y2 -=7
+            h2 +=4
 
-    cropped = cv2.resize(cropped,(weight,height))
+            cropped = image[y2:h2,x2:w2]
+            try:
+                #resize the crop image to 40x40 dimension
+                croppedMouth = cv2.resize(cropped,(40,40))
+            except cv2.error:
+                continue
+            #normalized Mouth coordinates
+            normMouth = normalizedMouth(croppedMouth)
 
-    model = read_model(path)
+            normMouth = np.array(normMouth[:, 0:, :2])
 
-    model.predict(cropped)
+            normMouth = normMouth.transpose(2, 0, 1).reshape(40,-1)
 
-    print(len(cropped))
+            normMouth = np.resize(normMouth, 1600)
 
-    cv2.imshow("Output", cropped)
+            wordSequence[wCounter] = normMouth
+            wCounter += 1
+
+            model.predict(wordSequence)
+                    #wCounter = 0
+                    #sentenceList.append(wordSequence)
+                    #wordSequence = np.zeros((40,1600))
+                    #wordSequence[wCounter] = normMouth
 
     cv2.waitKey(1)
-
-
-cv2.destroyAllWindows()
 cap.release()
