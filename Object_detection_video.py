c233240363c7078dd72f04df122867fc1d641b77
jadenD <delmojason@gmail.com>
2019-03-05 21:26:51 +0800

    updated sample run

--- Object_detection_video.py
+++ Object_detection_video.py
@@ -101,7 +101,7 @@ detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
 num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
 # Open video file
-video = cv2.VideoCapture(PATH_TO_VIDEO)
+video = cv2.VideoCapture(0)
 
 while(video.isOpened()):
 
@@ -115,6 +115,16 @@ while(video.isOpened()):
         [detection_boxes, detection_scores, detection_classes, num_detections],
         feed_dict={image_tensor: frame_expanded})
 
+    a = vis_util.visualize_boxes_and_labels_on_image_array(
+        frame,
+        np.squeeze(boxes),
+        np.squeeze(classes).astype(np.int32),
+        np.squeeze(scores),
+        category_index,
+        use_normalized_coordinates=True,
+        line_thickness=3,
+        min_score_thresh=0.80)
+
     #box =unnormalize_coordinates(area[0], area[1], area[2], area[3],image)
 
     #print(np.shape(image))
@@ -123,7 +133,7 @@ while(video.isOpened()):
 
     #normMouth = normalizedMouth(cropped)
 
-    cv2.imshow("mouth", frame)
+    cv2.imshow("mouth", a)
     print(boxes)
     cv2.waitKey(1)
     # All the results have been drawn on the frame, so it's time to display it.
--- sampleRunning.py
+++ sampleRunning.py
@@ -43,29 +43,50 @@ def normalizedMouth(mouth):
     return mouth
 
 def read_model(weights_filename='untrained_weight.h5',
-               mod='model-2.h5'):
+               model='model2.h5'):
 
     global path
 
-    print("Reading Model from " + weights_filename + " and " + mod)
+    print("Reading Model from " + model)
     print("Please wait, it takes time.")
-    model = load_model(path+'/'+mod)
-    #model.load_weights(weights_filename)
+    model = load_model(model)
+    #model.load_weights('C:/Users/Jaden/Downloads/GRIDcorpus-experiments-master/TRAINED-MODELS/weightts.hdf5')
     print("Finish Reading!")
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
     return model
 
-cap = cv2.VideoCapture(path)
+
+position = 0
+
+def putSub(frame,word):
+
+    global position
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    #position = (len(str(word)) + 5) + 280
+
+    cv2.putText(image, word, (280,220), font, 1, (0, 0, 0), 2)
+
+    return image
+
+
+
+vidFile = 'D:/Datasets/s1/lwir2n.mpg'
+cap = cv2.VideoCapture(vidFile)
 x2 = 0
 y2 = 0
 w2 = 0
 h2 = 0
 counter = 0
 
-wordSequence = np.zeros((40, 1600))
+wordSequence = np.zeros((1,40, 1600))
 wCounter = 0
 
 model = read_model()
+previous = 0
+wordDict = {'set': 36, 'i': 17, 'b': 3, 'u': 43, 'green': 15, 'five': 12, 'v': 44, 'blue': 5, 'm': 23, 'seven': 37, 'e': 9, 'red': 34, 'z': 49, 'place': 30, 'r': 33, 'q': 32, 'one': 28, 'at': 2, 'bin': 4, 'f': 11, 'with': 46, 'x': 47, 'now': 26, 'eight': 10, 'a': 0, 'please': 31, 'soon': 39, 'by': 6, 'in': 18, 't': 40, 'g': 14, 'c': 7, 'four': 13, 'zero': 50, 'o': 27, 'lay': 22, 'p': 29, 'again': 1, 'j': 19, 'n': 24, 'six': 38, 'two': 42, 'l': 21, 'd': 8, 'three': 41, 'h': 16, 'white': 45, 'nine': 25, 'y': 48, 'k': 20, 's': 35}
+
 
 while True:
     # Getting out image by webcam
@@ -92,42 +113,51 @@ while True:
         shape = predictor(gray, rect)
         shape = face_utils.shape_to_np(shape)
 
+        minn = np.asarray(shape[48:68])
 
-        if counter == 1:
-            minn = np.asarray(shape[48:68])
+        min2 = minn.min(0)
+        max2 = minn.max(0)
 
-            min2 = minn.min(0)
-            max2 = minn.max(0)
+        x2, y2 = min2
+        w2, h2 = max2
 
-            x2, y2 = min2
-            w2, h2 = max2
+        y2 -=7
+        h2 +=4
 
-            y2 -=7
-            h2 +=4
+        cropped = image[y2:h2,x2:w2]
+        try:
+            #resize the crop image to 40x40 dimension
+            croppedMouth = cv2.resize(cropped,(40,40))
+        except cv2.error:
+            continue
+        #normalized Mouth coordinates
+        normMouth = normalizedMouth(croppedMouth)
 
-            cropped = image[y2:h2,x2:w2]
-            try:
-                #resize the crop image to 40x40 dimension
-                croppedMouth = cv2.resize(cropped,(40,40))
-            except cv2.error:
-                continue
-            #normalized Mouth coordinates
-            normMouth = normalizedMouth(croppedMouth)
+        normMouth = np.array(normMouth[:, 0:, :2])
 
-            normMouth = np.array(normMouth[:, 0:, :2])
+        normMouth = normMouth.transpose(2, 0, 1).reshape(40,-1)
 
-            normMouth = normMouth.transpose(2, 0, 1).reshape(40,-1)
+        normMouth = np.resize(normMouth, 1600)
 
-            normMouth = np.resize(normMouth, 1600)
+        wordSequence[0][wCounter] = normMouth
+        wCounter += 1
 
-            wordSequence[wCounter] = normMouth
-            wCounter += 1
 
-            model.predict(wordSequence)
-                    #wCounter = 0
-                    #sentenceList.append(wordSequence)
-                    #wordSequence = np.zeros((40,1600))
-                    #wordSequence[wCounter] = normMouth
 
+        if wCounter == 6:
+            a = model.predict_classes(wordSequence)
+            if previous != a[0]:
+                word = list(wordDict.keys())[list(wordDict.values()).index(a[0])]
+                print(word)
+                image = putSub(image, word)
+
+            wordSequence = np.zeros((1, 40, 1600))
+
+
+
+            wCounter = 0
+            previous = a[0]
+
+    cv2.imshow('lo;',image)
     cv2.waitKey(1)
 cap.release()
